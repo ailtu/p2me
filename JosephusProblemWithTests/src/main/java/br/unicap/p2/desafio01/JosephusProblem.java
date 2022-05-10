a74e38686b2012dc909d344a56b6571639d032be
Ailton <ailtonc.n64@gmail.com>
2022-05-10 18:09:14 -0300

    josephus problem sucessed

--- JosephusProblemWithTests/src/main/java/br/unicap/p2/desafio01/JosephusProblem.java
+++ JosephusProblemWithTests/src/main/java/br/unicap/p2/desafio01/JosephusProblem.java
@@ -2,38 +2,36 @@ package br.unicap.p2.desafio01;
 
 public class JosephusProblem {
 
-    public static int[] solve(int m, int n) {
-
-        int[] positions = new int[n];
-        int[] seqEliminated = new int[n];
-        int countEliminated = 0;
-        int currentPos = -1;
-
-        while (countEliminated < n) {
-            countEliminated = moveNext(positions, n, m, currentPos);
-        }
-
-        seqEliminated[countEliminated] = currentPos;
-        positions[currentPos] = -1;
-        countEliminated++;
-
-        return seqEliminated;
-    }
-
-    public static int moveNext(int[] positions, int n, int m, int currentPos) {
-
-        int leaps = 0;
-
-        while (leaps < m) {
-            currentPos++;
-            currentPos = currentPos % m;
-            boolean isDead = positions[currentPos] == -1;
-
-            if (isDead) {
-                continue;
-            }
-            leaps++;
-        }
-        return currentPos;
-    }
+	public static int[] solve(int m, int n) {
+		
+		int[] positions = new int[n];
+		int[] sequenceEliminated = new int[n];
+		int countEliminated = 0;
+		int currentPosition = -1;	
+		
+		while(countEliminated < n) {
+			currentPosition = moveNext(positions, currentPosition, n, m);
+			positions[currentPosition] = -1;
+			sequenceEliminated[countEliminated] = currentPosition;
+			countEliminated++;
+		}		
+		return sequenceEliminated;		
+	}
+	
+	public static int moveNext(int[] positions, int currentPosition, int n, int m) {		
+		
+		int individuals = 0;		
+		
+		while(individuals < m) {			
+			currentPosition++;
+			currentPosition = currentPosition % n;
+			
+			//Already eliminated?
+			if(positions[currentPosition] == -1) {
+				continue;
+			}
+			individuals++;
+		}		
+		return currentPosition;			
+	}
 }
